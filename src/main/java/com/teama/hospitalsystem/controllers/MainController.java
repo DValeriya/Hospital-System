7225c8bd512388cf7df88d8c52e2c839fd5fc5dd
Lera <dulevets.v@gmail.com>
2021-10-21 22:26:18 +0300

    add test controller

---
+++ src/main/java/com/teama/hospitalsystem/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.teama.hospitalsystem.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
+    }
+}
