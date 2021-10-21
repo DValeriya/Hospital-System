e4763eb25302efb996050f2479acec80cb214ce6
Lera <dulevets.v@gmail.com>
2021-10-21 22:01:33 +0300

    init

---
+++ src/main/java/com/teama/hospitalsystem/HospitalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.teama.hospitalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teama/hospitalsystem/HospitalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teama.hospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
