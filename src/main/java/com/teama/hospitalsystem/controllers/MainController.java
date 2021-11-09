21c9b22685da803ea2a1868f1dac9f52ad991ac3
Dima Mihaylov <dima.mihaylov@opu.ua>
2021-11-09 19:44:20 +0200

    add changes

--- src/main/java/com/teama/hospitalsystem/controllers/MainController.java
+++ src/main/java/com/teama/hospitalsystem/controllers/MainController.java
@@ -1,11 +1,11 @@
 package com.teama.hospitalsystem.controllers;
 
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class MainController {
 
     @GetMapping("/")
