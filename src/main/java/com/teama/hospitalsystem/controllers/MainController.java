040aad85496414de1a5311a1af00303834312bad
Lera <dulevets.v@gmail.com>
2021-11-09 20:37:44 +0200

    OD21DEVW-34 Spring configuration. Fix heroku crash.

--- src/main/java/com/teama/hospitalsystem/controllers/MainController.java
+++ src/main/java/com/teama/hospitalsystem/controllers/MainController.java
@@ -1,11 +1,9 @@
 package com.teama.hospitalsystem.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class MainController {
 
     @GetMapping("/")
