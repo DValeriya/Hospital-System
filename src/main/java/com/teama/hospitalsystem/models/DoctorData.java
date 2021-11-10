6add8e8c757fa73d2195418ed88afd2651f23123
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2021-11-10 18:16:37 +0200

    OD21DEVW-38 DoctorData POJO implementation

--- src/main/java/com/teama/hospitalsystem/models/DoctorData.java
+++ src/main/java/com/teama/hospitalsystem/models/DoctorData.java
@@ -1,4 +1,66 @@
 package com.teama.hospitalsystem.models;
 
+import jdk.nashorn.internal.runtime.Specialization;
+
+import java.math.BigInteger;
+import java.sql.Time;
+import java.util.Objects;
+
 public class DoctorData {
+    private BigInteger doctorDataId;
+    private DoctorSpecialization spec;
+    private Time appointmentDuration;
+    /* Ctor */
+
+    public DoctorData(){}
+    /* Setters */
+
+    public void setDoctorDataId(BigInteger doctorDataId) {
+        this.doctorDataId = doctorDataId;
+    }
+
+    public void setSpec(DoctorSpecialization spec) {
+        this.spec = spec;
+    }
+
+    public void setAppointmentDuration(Time appointmentDuration) {
+        this.appointmentDuration = appointmentDuration;
+    }
+    /* Getters */
+
+    public BigInteger getDoctorDataId() {
+        return doctorDataId;
+    }
+
+    public DoctorSpecialization getSpec() {
+        return spec;
+    }
+
+    public Time getAppointmentDuration() {
+        return appointmentDuration;
+    }
+
+    @Override
+    public String toString() {
+        return "DoctorData{" +
+                "doctorDataId=" + doctorDataId +
+                ", spec=" + spec +
+                ", appointmentDuration=" + appointmentDuration +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DoctorData)) return false;
+        DoctorData that = (DoctorData) o;
+        return doctorDataId.equals(that.doctorDataId) &&
+                spec.equals(that.spec) &&
+                Objects.equals(appointmentDuration, that.appointmentDuration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(doctorDataId, spec, appointmentDuration);
+    }
 }
