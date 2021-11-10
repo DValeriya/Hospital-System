3e78acfa84f8f9e88312e85412103a825e408089
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2021-11-10 17:26:38 +0200

    Fix naming

--- src/main/java/com/teama/hospitalsystem/HospitalSystemApplication.java
+++ src/main/java/com/teama/hospitalsystem/HospitalSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalsystemApplication {
+public class HospitalSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalsystemApplication.class, args);
+		SpringApplication.run(HospitalSystemApplication.class, args);
 	}
 
 }
