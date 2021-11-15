f5e6849695f7b921251f039d0fc24d717d7a74b9
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2021-11-15 19:49:18 +0200

    AppointmentStatus enum changes

--- src/main/java/com/teama/hospitalsystem/util/AppointmentStatus.java
+++ src/main/java/com/teama/hospitalsystem/util/AppointmentStatus.java
@@ -1,7 +1,29 @@
 package com.teama.hospitalsystem.util;
 
+import com.teama.hospitalsystem.models.Appointment;
+
+import java.math.BigInteger;
+
 public enum AppointmentStatus {
-    Free,
-    Reserved,
-    NotAvailable
+    FREE(BigInteger.valueOf(1)),
+    RESERVED(BigInteger.valueOf(2)),
+    NOTAVAILABLE(BigInteger.valueOf(3));
+
+    private final BigInteger _id;
+    AppointmentStatus(BigInteger id){
+        _id = id;
+    }
+
+    public BigInteger getId() {
+        return _id;
+    }
+
+    public static AppointmentStatus fromId(BigInteger id) throws IllegalArgumentException {
+        for (AppointmentStatus status : AppointmentStatus.values()) {
+            if (status.getId().equals(id)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("invalid id");
+    }
 }
