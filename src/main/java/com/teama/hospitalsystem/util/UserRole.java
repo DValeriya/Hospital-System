92651a2aad10606bdede500d11e0598c1b7df9a2
Dima Mihaylov <dima.mihaylov@opu.ua>
2021-11-15 11:18:56 +0200

    add ID binding from DML script to enum UserRole

--- src/main/java/com/teama/hospitalsystem/util/UserRole.java
+++ src/main/java/com/teama/hospitalsystem/util/UserRole.java
@@ -1,8 +1,35 @@
 package com.teama.hospitalsystem.util;
 
+import java.math.BigInteger;
+
 public enum UserRole {
-    Admin,
-    Doctor,
-    Registry,
-    Patient
+    Admin(1),
+    Doctor(2),
+    Registry(3),
+    Patient(4);
+
+    private final int id;
+
+    UserRole(int i) {
+        id = i;
+    }
+
+    public int GetId() {
+        return id;
+    }
+
+    public static UserRole fromId(BigInteger id) throws IllegalArgumentException {
+        switch (id.intValue()) {
+            case 1:
+                return Admin;
+            case 2:
+                return Doctor;
+            case 3:
+                return Registry;
+            case 4:
+                return Patient;
+            default:
+                throw new IllegalArgumentException("invalid id");
+        }
+    }
 }
