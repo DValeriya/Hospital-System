d2f1154b863e90f826da0c697113211865812489
Alex <akolaev7@gmail.com>
2021-11-23 16:07:37 +0200

    OD21DEVW-41 EmployerDataDAO implementation. Added RowMapper in class

--- src/main/java/com/teama/hospitalsystem/dao/impl/EmployerDataDAOImpl.java
+++ src/main/java/com/teama/hospitalsystem/dao/impl/EmployerDataDAOImpl.java
@@ -2,10 +2,10 @@ package com.teama.hospitalsystem.dao.impl;
 
 import com.teama.hospitalsystem.dao.EmployerDataDAO;
 import com.teama.hospitalsystem.models.EmployerData;
-import com.teama.hospitalsystem.util.mappers.EmployerRowMapper;
 import com.teama.hospitalsystem.util.EmployerStatus;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,10 +25,15 @@ public class EmployerDataDAOImpl implements EmployerDataDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private RowMapper<EmployerData> rowMapper = (rs, rowNum) -> new EmployerData(
+        rs.getBigDecimal("ID").toBigInteger(),
+        rs.getDate("HIRING_DATE"),
+        EmployerStatus.fromId(rs.getBigDecimal("STATUS").toBigInteger()),
+        rs.getTime("START_WORKING_TIME"),
+        rs.getTime("END_WORKING_TIME"));
 
     @Override
     public void createEmployerData(EmployerData employerData, BigInteger userId) throws DataAccessException{
-
         SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName(CREATE_EMP_DATA_PROCEDURE);
         Map<String, Object> inParamMap = new HashMap<>();
 
@@ -41,13 +45,11 @@ public class EmployerDataDAOImpl implements EmployerDataDAO {
 
         SqlParameterSource in = new MapSqlParameterSource(inParamMap);
 
-
         jdbcCall.execute(in);
     }
 
     @Override
     public void editEmployerData (EmployerData employerData) {
-
         SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName(EDIT_EMPLOYER_DATA_PROCEDURE);
         Map<String, Object> inParamMap = new HashMap<>();
 
@@ -55,6 +57,7 @@ public class EmployerDataDAOImpl implements EmployerDataDAO {
         inParamMap.put("START_WORKING_TIME", employerData.getStartWorkingTime());
         inParamMap.put("END_WORKING_TIME", employerData.getEndWorkingTime());
         inParamMap.put("EMPLOYER_ID", employerData.getEmployerDataId());
+
         SqlParameterSource in = new MapSqlParameterSource(inParamMap);
 
         jdbcCall.execute(in);
@@ -67,29 +70,18 @@ public class EmployerDataDAOImpl implements EmployerDataDAO {
 
     @Override
     public EmployerData getEmployerDataByUserId(BigInteger userId) {
-        try{
-            return jdbcTemplate.queryForObject(GET_EMP_DATA_BY_USER_ID, new EmployerRowMapper(), userId);
-        } catch (DataAccessException e) {
-            return null;
-        }
+        return jdbcTemplate.queryForObject(GET_EMP_DATA_BY_USER_ID, rowMapper, userId);
+
     }
 
     @Override
     public Collection<EmployerData> getEmployerListByStatus(EmployerStatus employerStatus) {
-        try{
-            return jdbcTemplate.query(GET_EMP_LIST_BY_STATUS, new EmployerRowMapper(), employerStatus.getId());
-        } catch (DataAccessException e) {
-            return Collections.emptyList();
-        }
+        return jdbcTemplate.query(GET_EMP_LIST_BY_STATUS, rowMapper, employerStatus.getId());
     }
 
     @Override
     public BigInteger getEmployerDataId(BigInteger userId) {
-        try{
-            Integer id = jdbcTemplate.queryForObject(GET_EMP_DATA_ID_BY_USER_ID, Integer.class, userId);
-            return BigInteger.valueOf(id);
-        } catch (DataAccessException e) {
-            return userId;
-        }
+        Integer id = jdbcTemplate.queryForObject(GET_EMP_DATA_ID_BY_USER_ID, Integer.class, userId);
+        return BigInteger.valueOf(id);
     }
 }
