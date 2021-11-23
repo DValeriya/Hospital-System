8e39b8b1253f8009743395e7270f899f189ecc78
Dima Mihaylov <dima.mihaylov@opu.ua>
2021-11-23 12:04:00 +0200

    Updating UserRole implementation

--- src/main/java/com/teama/hospitalsystem/util/UserRole.java
+++ src/main/java/com/teama/hospitalsystem/util/UserRole.java
@@ -1,6 +1,9 @@
 package com.teama.hospitalsystem.util;
 
 import java.math.BigInteger;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum UserRole {
     ADMIN(BigInteger.valueOf(1)),
@@ -9,6 +12,10 @@ public enum UserRole {
     PATIENT(BigInteger.valueOf(4));
 
     private final BigInteger id;
+    private static final String ROLE_ID_NOT_FOUND = "User role with ID[%s] not found";
+
+    private static final Map<BigInteger, UserRole> ID_TO_ROLE = Stream.of(values())
+            .collect(Collectors.toMap(UserRole::getId, role -> role));
 
     UserRole(BigInteger id) {
         this.id = id;
@@ -19,11 +26,10 @@ public enum UserRole {
     }
 
     public static UserRole fromId(BigInteger id) throws IllegalArgumentException {
-        for (UserRole role : UserRole.values()) {
-            if (role.getId().compareTo(id) == 0) {
-                return role;
-            }
+        UserRole role = ID_TO_ROLE.get(id);
+        if (role == null) {
+            throw new IllegalArgumentException(String.format(ROLE_ID_NOT_FOUND, id));
         }
-        throw new IllegalArgumentException("invalid id");
+        return role;
     }
 }
