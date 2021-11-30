f4482a79ae3887f5545ee889378c6a4eafb3d8b2
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2021-11-30 14:28:06 +0200

    AppointmentDAO fixes

--- src/main/java/com/teama/hospitalsystem/dao/impl/AppointmentDAOImpl.java
+++ src/main/java/com/teama/hospitalsystem/dao/impl/AppointmentDAOImpl.java
@@ -1,10 +1,12 @@
 package com.teama.hospitalsystem.dao.impl;
 
 import com.teama.hospitalsystem.dao.AppointmentDAO;
+import com.teama.hospitalsystem.exceptions.DAOException;
 import com.teama.hospitalsystem.models.Appointment;
 import com.teama.hospitalsystem.models.WorkDay;
 import com.teama.hospitalsystem.util.AppointmentStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,109 +15,183 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Repository
 public class AppointmentDAOImpl implements AppointmentDAO {
-    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
 
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppointmentDAOImpl.class);
     private final JdbcTemplate jdbcTemplate;
 
     public AppointmentDAOImpl(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    private final RowMapper<Appointment> rowMapper = (rs, rowNum) -> {
-        BigInteger id = BigInteger.valueOf(rs.getLong("ID"));
-        BigInteger doctorId = BigInteger.valueOf(rs.getLong("DOCTORID"));
-        return new Appointment.Builder(id, rs.getTimestamp("EXPSTART"), rs.getTimestamp("EXPEND"), doctorId,
-                AppointmentStatus.fromId(BigInteger.valueOf(rs.getInt("STATUS"))))
-                .withActualEnd(rs.getTimestamp("ACTEND"))
-                .withActualStart(rs.getTimestamp("ACTSTART"))
-                .withPatientId(BigInteger.valueOf(rs.getLong("PATIENTID")))
-                .withDiagnosis(rs.getString("DIAGNOSIS"))
-                .withReferral(rs.getString("REFERRAL"))
-                .withNextAppointment(BigInteger.valueOf(rs.getLong("NXTAPP")))
+    private final RowMapper<Appointment> rowMapper  = (rs, rowNum)  -> {
+        BigInteger id = BigInteger.valueOf(rs.getLong(ID));
+        BigInteger doctorId = BigInteger.valueOf(rs.getLong(DOCTOR_ID));
+        return new Appointment.Builder(id, rs.getTimestamp(EXPSTART), rs.getTimestamp(EXPEND), doctorId,
+                AppointmentStatus.fromId(BigInteger.valueOf(rs.getInt(STATUS))))
+                .withActualEnd(rs.getTimestamp(ACTEND))
+                .withActualStart(rs.getTimestamp(ACTSTART))
+                .withPatientId(BigInteger.valueOf(rs.getLong(PATIENT_ID)))
+                .withDiagnosis(rs.getString(DIAGNOSIS))
+                .withReferral(rs.getString(REFERRAL))
+                .withNextAppointment(BigInteger.valueOf(rs.getLong(NXTAPP)))
                 .build();
     };
 
     @Override
-    public void createAppointment(Appointment appointment) throws DataAccessException {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
-                .withProcedureName("Create_Appointment");
-        Map<String, Object> params = new HashMap<>();
-        params.put("EXPSTART", sdf.format(appointment.getExpectedStart()));
-        params.put("EXPEND", sdf.format(appointment.getExpectedEnd()));
-        if (appointment.getActualStart() != null) {
-            params.put("ACTSTART", sdf.format(appointment.getActualStart()));
-        }else  params.put("ACTSTART", (appointment.getActualStart()));
-        if (appointment.getActualEnd() != null){
-            params.put("ACTEND", sdf.format(appointment.getActualEnd()));
-        }else params.put("ACTEND", (appointment.getActualEnd()));
-        params.put("DOCTORID", appointment.getDoctorId());
-        params.put("PATIENTID", appointment.getPatientId());
-        params.put("DIAGNOSIS", appointment.getDiagnosis());
-        params.put("REFFERAL", appointment.getReferral());
-        params.put("NXTAPP", appointment.getNextAppointment());
-        params.put("STATUS", String.valueOf(appointment.getStatus().getId()));
-        SqlParameterSource in = new MapSqlParameterSource(params);
-        jdbcCall.execute(in);
+    public Appointment createAppointment(Appointment appointment) throws DataAccessException {
+        try{
+            SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                    .withFunctionName("Create_Appointment");
+            SqlParameterSource in = new MapSqlParameterSource(fillMap(appointment));
+            appointment.setId(jdbcCall.executeFunction(BigDecimal.class,in).toBigInteger());
+            return appointment;
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Appointment was not created.");
+        }
+
     }
 
     @Override
     public Appointment getAppointmentById(BigInteger id) throws DataAccessException {
-        return jdbcTemplate.queryForObject(SQL_GET_BY_ID, rowMapper, id);
+        try{
+            return jdbcTemplate.queryForObject(SQL_GET_BY_ID, rowMapper, id);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by id.");
+        }
     }
 
     @Override
     public Collection<Appointment> getAppointmentByDoctorId(BigInteger doctorId) throws DataAccessException {
-        return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID, rowMapper, doctorId);
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID, rowMapper, doctorId);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by doctorId.");
+        }
     }
 
     @Override
     public Collection<Appointment> getAppointmentByDoctorIdForADay(BigInteger doctorId, WorkDay day) throws DataAccessException {
-        return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID_AND_DAY, rowMapper, doctorId,
-                        sdf.format(day.getDate()) + '*');
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID_AND_DAY, rowMapper, day.getWorkDayId(),
+                    doctorId);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by doctorId and by WorkDay.");
+        }
     }
 
     @Override
     public Collection<Appointment> getAppointmentByDoctorIdForAMonth(BigInteger doctorId, int month, int year) throws DataAccessException {
-        return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID_AND_DAY, rowMapper, doctorId, String.format("[1-31]-%s-%s*", month, year));
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_DOCTOR_ID_AND_DAY, rowMapper, doctorId, String.format("[1-31]-%s-%s*", month, year));
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to ged information by doctorId and Date");
+        }
     }
 
     @Override
     public Collection<Appointment> getAppointmentByPatientId(BigInteger patientId) throws DataAccessException {
-        return jdbcTemplate.query(SQL_GET_BY_PATIENT_ID, rowMapper, patientId);
+
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_PATIENT_ID, rowMapper, patientId);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by patientId.");
+        }
     }
 
     @Override
     public Collection<Appointment> getAppointmentByPatientIdForADate(BigInteger patientId, Date date) throws DataAccessException {
-        return jdbcTemplate.query(SQL_GET_BY_PATIENT_ID_AND_DATE, rowMapper, patientId,
-                        sdf.format(date) + '*');
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_PATIENT_ID_AND_DATE, rowMapper, patientId,
+                    sdf.format(date) + '*');
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by patientId and Date.");
+        }
     }
 
     @Override
     public void changeAppointmentStatus(BigInteger appointmentId, AppointmentStatus status) throws DataAccessException {
-        jdbcTemplate.update(SQL_CHANGE_APPOINTMENT_STATUS, status.getId(), appointmentId);
+        try{
+            jdbcTemplate.update(SQL_CHANGE_APPOINTMENT_STATUS, status.getId(), appointmentId);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to change appointment status.");
+        }
     }
 
     @Override
-    public void editAppointment(Map<String, Object> params) throws DataAccessException {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
-                .withProcedureName("Update_Appointment");
-        SqlParameterSource in = new MapSqlParameterSource(params);
-        jdbcCall.execute(in);
+    public Collection<Appointment> getAppointmentByWorkDay(WorkDay day) throws DataAccessException {
+        try{
+            return jdbcTemplate.query(SQL_GET_BY_WORKDAY, rowMapper, day.getWorkDayId());
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to get information by WorkDay.");
+        }
     }
 
     @Override
-    public void startAppointment(BigInteger appointmentId) {
-        jdbcTemplate.update(SQL_APPOINTMENT_MANIPULATION, 21, appointmentId, Calendar.getInstance().getTime());
+    public void editAppointment(Appointment appointment) throws DataAccessException {
+        try{
+            SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                    .withProcedureName("Update_Appointment");
+            SqlParameterSource in = new MapSqlParameterSource(fillMap(appointment));
+            jdbcCall.execute(in);
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to edit information by id.");
+        }
     }
 
     @Override
-    public void endAppointment(BigInteger appointmentId) {
-        jdbcTemplate.update(SQL_APPOINTMENT_MANIPULATION, 22, appointmentId, Calendar.getInstance().getTime());
+    public void startAppointment(BigInteger appointmentId) throws DataAccessException {
+        try{
+            jdbcTemplate.update(SQL_APPOINTMENT_MANIPULATION, 21, appointmentId, Calendar.getInstance().getTime());
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to start appointment.");
+        }
+    }
+
+    @Override
+    public void endAppointment(BigInteger appointmentId) throws DataAccessException {
+        try{
+            jdbcTemplate.update(SQL_APPOINTMENT_MANIPULATION, 21, appointmentId, Calendar.getInstance().getTime());
+        }catch (DataAccessException ex){
+            LOGGER.error(ex.getMessage());
+            throw new DAOException("AppointmentDAOImpl error. Failed to end appointment.");
+        }
+    }
+
+    private Map<String, Object> fillMap(Appointment appointment){
+        Map<String, Object> toReturn = new HashMap<>();
+        toReturn.put(EXPSTART, sdf.format(appointment.getExpectedStart()));
+        toReturn.put(EXPEND, sdf.format(appointment.getExpectedEnd()));
+        if (appointment.getActualStart() != null) {
+            toReturn.put(ACTSTART, sdf.format(appointment.getActualStart()));
+        }else  toReturn.put(ACTSTART, (appointment.getActualStart()));
+        if (appointment.getActualEnd() != null){
+            toReturn.put(ACTEND, sdf.format(appointment.getActualEnd()));
+        }else toReturn.put(ACTEND, (appointment.getActualEnd()));
+        toReturn.put(DOCTOR_ID, appointment.getDoctorId());
+        toReturn.put(PATIENT_ID, appointment.getPatientId());
+        toReturn.put(DIAGNOSIS, appointment.getDiagnosis());
+        toReturn.put(REFERRAL, appointment.getReferral());
+        toReturn.put(NXTAPP, appointment.getNextAppointment());
+        toReturn.put(STATUS, String.valueOf(appointment.getStatus().getId()));
+        return toReturn;
     }
 }
