fff6af1450d963fd1247e7e5d6b347c9d964b2ca
Lera <dulevets.v@gmail.com>
2021-12-02 01:36:53 +0200

    changing the return type of a method in DoctorSpecializationService

--- src/main/java/com/teama/hospitalsystem/services/DoctorSpecializationService.java
+++ src/main/java/com/teama/hospitalsystem/services/DoctorSpecializationService.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 
 public interface DoctorSpecializationService {
 
-    void createDoctorSpecialization(DoctorSpecialization spec);
+    DoctorSpecialization createDoctorSpecialization(DoctorSpecialization spec);
     void deleteDoctorSpecialization(DoctorSpecialization spec);
     DoctorSpecialization getDoctorSpecializationById(BigInteger id);
     Collection<DoctorSpecialization> getDoctorSpecializationList();
--- src/main/java/com/teama/hospitalsystem/services/impl/DoctorSpecializationServiceImpl.java
+++ src/main/java/com/teama/hospitalsystem/services/impl/DoctorSpecializationServiceImpl.java
@@ -1,11 +1,12 @@
 package com.teama.hospitalsystem.services.impl;
 
 import com.teama.hospitalsystem.dao.DoctorSpecializationDAO;
-import com.teama.hospitalsystem.exceptions.EntityNotFoundException;
+import com.teama.hospitalsystem.exceptions.DAOException;
 import com.teama.hospitalsystem.models.DoctorSpecialization;
 import com.teama.hospitalsystem.services.DoctorSpecializationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
@@ -15,28 +16,40 @@ import java.util.Collection;
 public class DoctorSpecializationServiceImpl implements DoctorSpecializationService {
 
     private final DoctorSpecializationDAO dao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DoctorSpecializationServiceImpl.class);
 
     public DoctorSpecializationServiceImpl(DoctorSpecializationDAO dao) {
         this.dao = dao;
     }
 
     @Override
-    public void createDoctorSpecialization(DoctorSpecialization spec) throws DataAccessException {
-        dao.createDoctorSpecialization(spec);
+    public DoctorSpecialization createDoctorSpecialization(DoctorSpecialization spec) throws DAOException {
+        try {
+            BigInteger getNewId = dao.createDoctorSpecialization(spec);
+            return getDoctorSpecializationById(getNewId);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
     }
 
     @Override
-    public void deleteDoctorSpecialization(DoctorSpecialization spec) throws DataAccessException {
-        dao.deleteDoctorSpecialization(spec);
+    public void deleteDoctorSpecialization(DoctorSpecialization spec) throws DAOException {
+        try {
+            dao.deleteDoctorSpecialization(spec);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
     }
 
     @Override
-    public DoctorSpecialization getDoctorSpecializationById(BigInteger id)
-            throws DataAccessException, EntityNotFoundException {
+    public DoctorSpecialization getDoctorSpecializationById(BigInteger id) throws DAOException{
         try {
             return dao.getDoctorSpecializationByDoctorDataId(id);
-        } catch (EmptyResultDataAccessException ex) {
-            throw new EntityNotFoundException();
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
         }
     }
 
