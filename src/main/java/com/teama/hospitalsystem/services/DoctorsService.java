edab28b66dae3c3fb3de421f97bdf32411772bb8
Lera <dulevets.v@gmail.com>
2021-12-02 12:17:57 +0200

    OD21DEVW-59 DoctorsService implementation

--- src/main/java/com/teama/hospitalsystem/services/DoctorsService.java
+++ src/main/java/com/teama/hospitalsystem/services/DoctorsService.java
@@ -1,4 +1,14 @@
 package com.teama.hospitalsystem.services;
 
+import com.teama.hospitalsystem.models.DoctorData;
+
+import java.math.BigInteger;
+import java.util.List;
+
 public interface DoctorsService {
+    public DoctorData createDoctor(DoctorData doctorData, BigInteger employerId);
+    public void editDoctor(DoctorData doctorData);
+    public DoctorData getDoctorDataById(BigInteger id);
+    public DoctorData getDoctorDataId(BigInteger id);
+    public List<DoctorData> getDoctorListBySpecialization(BigInteger specializationId);
 }
--- src/main/java/com/teama/hospitalsystem/services/impl/DoctorsServiceImpl.java
+++ src/main/java/com/teama/hospitalsystem/services/impl/DoctorsServiceImpl.java
@@ -1,10 +1,75 @@
 package com.teama.hospitalsystem.services.impl;
 
+import com.teama.hospitalsystem.dao.DoctorDataDAO;
+import com.teama.hospitalsystem.exceptions.DAOException;
+import com.teama.hospitalsystem.models.DoctorData;
+import com.teama.hospitalsystem.services.DoctorSpecializationService;
 import com.teama.hospitalsystem.services.DoctorsService;
-import org.springframework.context.annotation.Scope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @Service
-@Scope("singleton")
 public class DoctorsServiceImpl implements DoctorsService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DoctorsServiceImpl.class);
+    private final DoctorDataDAO doctor;
+    private final DoctorSpecializationService doctorSpecializationService;
+
+    public DoctorsServiceImpl(DoctorDataDAO doctor, DoctorSpecializationService doctorSpecializationService) {
+        this.doctor = doctor;
+        this.doctorSpecializationService = doctorSpecializationService;
+    }
+
+    @Override
+    public DoctorData createDoctor(DoctorData doctorData, BigInteger employerId) throws DAOException{
+        try {
+            BigInteger getNewId = doctor.createDoctorData(doctorData, employerId); //TODO
+            return getDoctorDataId(getNewId);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
+    }
+
+    @Override
+    public void editDoctor(DoctorData doctorData) throws DAOException{
+        try {
+            doctor.editDoctorData(doctorData);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
+    }
+
+    @Override
+    public DoctorData getDoctorDataById(BigInteger id) throws DAOException {
+        try {
+            DoctorData doctorData = doctor.getDoctorDataByDoctorId(id);
+            doctorSpecializationService.getDoctorSpecializationById(id);
+            return doctorData;
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
+    }
+
+    @Override
+    public DoctorData getDoctorDataId(BigInteger id) throws DAOException {
+        try {
+            DoctorData doctorData = doctor.getDoctorDataId(id);
+            doctorSpecializationService.getDoctorSpecializationById(id);
+            return doctorData;
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getLocalizedMessage(), daoException);
+            throw daoException;
+        }
+    }
+
+    @Override
+    public List<DoctorData> getDoctorListBySpecialization(BigInteger specializationId) {
+        return doctor.getDoctorListBySpecialization(specializationId);
+    }
 }
