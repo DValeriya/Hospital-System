fa9b3856cc1e7cf5e3b2543eb67ebde3268874bc
Lera <dulevets.v@gmail.com>
2021-12-08 15:52:58 +0200

    OD21DEVW-67 DoctorsController implementation

---
+++ src/main/java/com/teama/hospitalsystem/controllers/DoctorsController.java
@@ -0,0 +1,67 @@
+package com.teama.hospitalsystem.controllers;
+
+import com.teama.hospitalsystem.exceptions.DAOException;
+import com.teama.hospitalsystem.models.DoctorData;
+import com.teama.hospitalsystem.services.DoctorsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorsController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DoctorsController.class);
+    private final DoctorsService doctorsService;
+
+    public DoctorsController(DoctorsService doctorsService) {
+        this.doctorsService = doctorsService;
+    }
+
+    @PostMapping("/{id}/create")
+    public ResponseEntity<?> createDoctor(@PathVariable BigInteger id, @RequestBody DoctorData doctorData) {
+        try {
+            DoctorData doctor = doctorsService.createDoctor(doctorData, id);
+            return ResponseEntity.ok(doctor);
+        } catch (DAOException daoException) {
+            return ResponseEntity.badRequest().body(daoException.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}/edit")
+    public ResponseEntity<?> editDoctor(@PathVariable BigInteger id, @RequestBody DoctorData doctorData) {
+        try {
+            DoctorData doctor = doctorsService.getDoctorDataById(id);
+
+            doctor.setAppointmentDuration(doctorData.getAppointmentDuration());
+
+            doctorsService.editDoctor(doctor);
+
+            return ResponseEntity.ok(doctor);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getMessage(), daoException, daoException.getCause());
+            return ResponseEntity.badRequest().body(daoException.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getDoctorDataById(@PathVariable BigInteger id) {
+        try {
+            System.out.println(id);
+            DoctorData doctorData = doctorsService.getDoctorDataById(id);
+            return ResponseEntity.ok(doctorData);
+        } catch (DAOException daoException) {
+            LOGGER.error(daoException.getMessage(), daoException, daoException.getCause());
+            return ResponseEntity.badRequest().body(daoException.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getDoctorsBySpecialization(@RequestParam("spec-id") BigInteger specializationId) {
+        List<DoctorData> doctorListBySpecialization = doctorsService.getDoctorListBySpecialization(specializationId);
+        return ResponseEntity.ok(doctorListBySpecialization);
+    }
+}
