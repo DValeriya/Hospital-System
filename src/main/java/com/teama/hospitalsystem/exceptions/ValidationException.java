7d646a34646cd3831a1337acf8534b9f330ba044
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2021-12-09 20:40:54 +0200

    Some finery

--- src/main/java/com/teama/hospitalsystem/exceptions/ValidationException.java
+++ src/main/java/com/teama/hospitalsystem/exceptions/ValidationException.java
@@ -7,28 +7,26 @@ import java.util.Set;
 
 public class ValidationException extends RuntimeException {
     private Map<String, String> map;
+    private StringBuilder formattedMessage;
     public ValidationException(){
         super();
     }
     public <T> ValidationException(Set<ConstraintViolation<T>> violations){
         super(String.format("{%d} violations has been found", violations.size()));
         Map<String, String> map = new HashMap<>();
+        formattedMessage = new StringBuilder("Founded violations:\n");
         for (ConstraintViolation<?> v: violations
         ) {
             map.put(v.getPropertyPath().toString(), v.getMessage() + " : " + v.getInvalidValue());
-//            toReturn.append("\t in ").append(v.getRootBeanClass().toString()).append(" : ").append(v.getMessage())
-//                    .append("\n\t\t").append(v.getPropertyPath()).append(" : ").append(v.getInvalidValue()).append('\n');
+            formattedMessage.append("\t in ").append(v.getRootBeanClass().toString()).append(" : ").append(v.getMessage())
+                    .append("\n\t\t").append(v.getPropertyPath()).append(" : ").append(v.getInvalidValue()).append('\n');
         }
         this.map = map;
     }
-    public Map<?,?> GetViolationsParams(){
+    public Map<String, String> GetViolationsParams(){
         return map;
     }
-//    private static <T> String formMessage(Set<ConstraintViolation<T>> violations){
-//        Map<String, String> map = new HashMap<>();
-////        StringBuilder toReturn = new StringBuilder("Such violations has been found:\n");
-//
-//        this.map = map;
-////        return toReturn.toString();
-//    }
+    public String getFormattedMessage() {
+        return formattedMessage.toString();
+    }
 }
